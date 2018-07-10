d4cfeb59aa99d0cc16044c9faa9284d63313d89e
James D. Bartlett III <jimmyb@gmail.com>
2018-07-10 13:17:11 -0500

    added comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -5,8 +5,8 @@ $(function() {
         * and that it is not empty.
         */
       it('are defined.', () => {
-          expect(allFeeds).toBeDefined();
-          expect(allFeeds.length).not.toBe(0);
+          expect(allFeeds).toBeDefined(); // expect allFeeds to be defined,
+          expect(allFeeds.length).not.toBe(0); // and its length not to be 0.
       });
 
       /* Loop through each feed
@@ -14,9 +14,9 @@ $(function() {
          * and that the URL is not empty.
          */
        it('have defined URLs, and they are not empty.', () => {
-         allFeeds.forEach(i => {
-           expect(i).toBeDefined();
-           expect(i.length).not.toBe(0);
+         allFeeds.forEach(i => { // for each feed in allFeeds,
+           expect(i).toBeDefined(); // expect the feed to be defined,
+           expect(i.length).not.toBe(0); // and its length not to be 0.
          });
        });
 
@@ -25,9 +25,9 @@ $(function() {
          * and that the name is not empty.
          */
        it('have defined names, and they are not empty.', () => {
-         allFeeds.forEach(i => {
-           let name = i.name;
-           expect(name).toBeDefined();
+         allFeeds.forEach(i => { // for each feed in allFeeds,
+           let name = i.name; // get 'name' property from 'i' and store in 'name' var
+           expect(name).toBeDefined(); // run same checks as before
            expect(name.length).not.toBe(0);
          });
        });
@@ -40,20 +40,20 @@ $(function() {
        * hidden by default.
        */
       it('element is hidden by default.', () => {
-        expect($('body').attr('class')).toBe('menu-hidden');
+        expect($('body').attr('class')).toBe('menu-hidden'); // expect body to have CSS class 'menu-hidden' by default
       });
 
       /* Ensure the menu changes
         * visibility when the menu icon is clicked.
         */
       it('should become visible when clicked, and then invisible when clicked again.', () => {
-        let clickIcon = () => {
+        let clickIcon = () => { // define a function that clicks the menu icon whenever it is called.
           $('.icon-list').click();
         };
-        expect($('body').hasClass('menu-hidden')).toBe(true);
-        clickIcon();
-        expect($('body').hasClass('menu-hidden')).toBe(false);
-        clickIcon();
+        expect($('body').hasClass('menu-hidden')).toBe(true); // check that body still has 'menu-hidden' class
+        clickIcon(); // click the icon,
+        expect($('body').hasClass('menu-hidden')).toBe(false); // now make sure the menu is visisble,
+        clickIcon(); // and click the icon to make it invisible again.
       });
     });
 
@@ -63,13 +63,14 @@ $(function() {
        * function is called and completes its work, there is at least
        * a single .entry element within the .feed container.
        */
-      beforeEach(done => {
-        loadFeed(0, () => {
+      beforeEach(done => { // before each test in this suite,
+        loadFeed(0, () => { // set up async callback.
           done();
         });
       });
 
       it('After loadFeed is called and finishes, the .feed container should have at least one .entry element.', done => {
+        // make sure .feed has a .entry element, and its length is not zero.
         expect($('.feed').find('.entry').length !== 0).toBe(true);
         done();
       });
@@ -80,11 +81,12 @@ $(function() {
       /* Ensure when a new feed is loaded
        * by the loadFeed function that the content actually changes.
        */
+      // declare feed0 and feed1 at this level so they are accessible throughout the rest of the function.
       let feed0, feed1;
-      beforeEach(done => {
-        loadFeed(0, () => {
+      beforeEach(done => { // before each test in this suite,
+        loadFeed(0, () => { // set up async callback,
           feed0 = $('.feed');
-          loadFeed(1, () => {
+          loadFeed(1, () => { // and load feed1 within the feed0 function to make sure it returns with data
             feed1 = $('.feed');
             done();
           });
@@ -93,7 +95,7 @@ $(function() {
       });
 
       it('When a new feed is loaded, the content should actually change.', done => {
-        expect(feed0 === feed1).toBe(false);
+        expect(feed0 === feed1).toBe(false); // check to make sure feed0 and feed1 are different.
         done();
       });
     });
